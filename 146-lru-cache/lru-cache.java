23f9f36791a5fd64bf08d24d92cc578b9ec4fce3
PaulShervin <paulshervin77@gmail.com>
2026-03-03 18:40:55 +0530

    Time: 44 ms (84.17%) | Memory: 131.4 MB (53.99%) - LeetSync

---
+++ 146-lru-cache/lru-cache.java
@@ -0,0 +1,72 @@
+class LRUCache {
+    class Node{
+        int key;
+        int val;
+        Node prev;
+        Node next;
+        Node(int key,int val)
+        {
+            this.key=key;
+            this.val=val;
+        }
+    }
+    private int cc;
+    private HashMap<Integer,Node> map;
+    private Node head;
+    private Node tail;
+    public LRUCache(int cc) {
+        this.cc=cc;
+        map=new HashMap<>();
+        head=new Node(0,0);
+        tail=new Node(0,0);
+
+        head.next=tail;
+        tail.prev=head;
+    }
+    public void insert_front(Node new_node)
+    {
+        new_node.next=head.next;
+        new_node.prev=head;
+        head.next.prev=new_node;
+        head.next=new_node;
+    }
+    public void remove_ele(Node node)
+    {
+        node.prev.next=node.next;
+        node.next.prev=node.prev;
+    }
+    public int get(int key) {
+        if(!map.containsKey(key))
+        {
+            return -1;
+        }
+
+        Node node=map.get(key);
+        remove_ele(node);
+        insert_front(node);
+
+        return node.val;
+    }
+    
+    public void put(int key, int value) {
+        if(map.containsKey(key))
+        {
+            Node node=map.get(key);
+            node.val=value;
+            remove_ele(node);
+            insert_front(node);   
+        }
+        else{
+            if(map.size()==cc)
+            {
+                Node least_recent_used=tail.prev;
+                remove_ele(least_recent_used);
+                map.remove(least_recent_used.key);
+            }
+
+            Node new_node=new Node(key,value);
+            insert_front(new_node);
+            map.put(key,new_node);
+        }
+    }
+}
